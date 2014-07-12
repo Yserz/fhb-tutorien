661fe139652bec520e682e14dbe73141540fc2f1
Yserz <michael.koppen@googlemail.com>
2014-07-12 20:23:53 +0200

    - fixed redirect

--- src/main/java/com/welovecoding/tutorial/view/Pages.java
+++ src/main/java/com/welovecoding/tutorial/view/Pages.java
@@ -8,7 +8,7 @@ public class Pages {
   public static final String ADMIN_POST_LOGIN = "/admin/index?faces-redirect=true";
   public static final String ADMIN_AUTHORS = "/admin/authors";
   public static final String ADMIN_CATEGORY = "/admin/categories";
-  public static final String ADMIN_INDEX = "/index";
+  public static final String ADMIN_INDEX = "/admin/index";
   public static final String ADMIN_PLAYLISTS = "/admin/playlists";
   public static final String ADMIN_VIDEOS = "/admin/videos";
   public static final String ADMIN_USERS = "/admin/users";
